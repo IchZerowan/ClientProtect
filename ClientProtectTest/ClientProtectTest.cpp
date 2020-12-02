ee56316c14b9de5791065d7fe0f8a0ebfd5cc5d9
IchZerowan <Ih01@i.ua>
2020-12-03 01:31:47 +0200

    Updated test project with command line arguments

--- ClientProtectTest/ClientProtectTest.cpp
+++ ClientProtectTest/ClientProtectTest.cpp
@@ -3,20 +3,48 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    const char* filename = "example.bin";
-    string str = "Hello, protected stringssss!";
+    const char* usage = " usage: .\\ClientProtectTest [encode|decode] filename";
 
-    // Writing string to the file
-    cout << "Writing string to the file" << endl;
-    cp_file_write_bytes(filename, (char*) str.c_str(), str.size());
-    cout << "String written to the file" << endl << endl;
+    if (argc < 3) {
+        cout << "No arguments provided," << usage << endl;
+        return -1;
+    }
+    char* filename = argv[2];
+
+    if (strcmp(argv[1], "encode") == 0) {
+        // Writing string to the file
+        cout << "Enter the string to save: ";
+        char buffer[500];
+        cin.getline(buffer, 500);
+        cout << "Writing string to the file" << endl;
+        cp_file_write_bytes(filename, buffer, strlen(buffer));
+        cout << "String written to the file" << endl << endl;
+    }
+
+    else if (strcmp(argv[1], "decode") == 0) {
+        // Reading string from the file
+        cout << "Reading string from the file" << endl;
+        int outSize = 0;
+        char* result = cp_file_read_bytes(filename, outSize);
+        cout << outSize << " bytes read from file:" << endl << result << endl << endl;
+    }
+
+    else if (strcmp(argv[1], "test") == 0) {
+        string test = "Hello, World!";
+        cout << "Writing string to the file" << endl;
+        cp_file_write_bytes(filename, (char*) test.c_str(), test.size());
+        cout << "String written to the file" << endl << endl;
+        cout << "Reading string from the file" << endl;
+        int outSize = 0;
+        char* result = cp_file_read_bytes(filename, outSize);
+        cout << outSize << " bytes read from file:" << endl << result << endl << endl;
+    }
+
+    else {
+        cout << "Invalid action," << usage << endl;
+        return -1;
+    }
     
-    // Reading string from the file
-    cout << "Reading string from the file:" << endl;
-    int outSize = 0;
-    char* result = cp_file_read_bytes(filename, outSize);
-
-    cout << result << " | " << outSize << " bytes" << endl;
 }
